fb344fa087a0cc42f19d4c1363aea4d7c71cf693
LaNice Powell <lanicemp@aol.com>
2019-06-27 11:20:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,3 @@
-'require_relative'../calculator.rb'
-
-# Add your variables here 
-
-first_number = "1.0" 
-second_number = "2.0"
+first_number = 1.0 
+second_number = 2.0
+sum === first_number + second_number
